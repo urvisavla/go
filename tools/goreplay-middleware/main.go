f2470fcd7ed8d985a2b6d1c9531107eef1af96af
Bartek Nowotarski <bartek@nowotarski.info>
2022-08-02 15:25:00 +0200

    tools/goreplay-middleware: Add goreplay middleware

---
+++ tools/goreplay-middleware/main.go
@@ -0,0 +1,84 @@
+// Based on https://github.com/buger/goreplay/blob/master/examples/middleware/token_modifier.go
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/hex"
+	"os"
+	"time"
+)
+
+// maxPerSecond defines how many requests should be checked at max per second
+const maxPerSecond = 1
+
+const (
+	requestType          byte = '1'
+	originalResponseType byte = '2'
+	replayedResponseType byte = '3'
+)
+
+var lastCheck = time.Now()
+var reqsCheckedPerSeq = 0
+var pendingRequests map[string]*Request
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		encoded := scanner.Bytes()
+		buf := make([]byte, len(encoded)/2)
+		hex.Decode(buf, encoded)
+
+		process(buf)
+	}
+}
+
+func process(buf []byte) {
+	// First byte indicate payload type:
+	payloadType := buf[0]
+	headerSize := bytes.IndexByte(buf, '\n') + 1
+	header := buf[:headerSize-1]
+
+	// Header contains space separated values of: request type, request id, and request start time (or round-trip time for responses)
+	meta := bytes.Split(header, []byte(" "))
+	// For each request you should receive 3 payloads (request, response, replayed response) with same request id
+	reqID := string(meta[1])
+	payload := buf[headerSize:]
+
+	switch payloadType {
+	case requestType:
+		if time.Since(lastCheck) > time.Second {
+			reqsCheckedPerSeq = 0
+			lastCheck = time.Now()
+		}
+
+		if reqsCheckedPerSeq < maxPerSecond {
+			pendingRequests[reqID] = &Request{
+				Headers: string(buf),
+			}
+			reqsCheckedPerSeq++
+		}
+
+		// Emitting data back, without modification
+		hexEncoder := hex.NewEncoder(os.Stdout)
+		hexEncoder.Write(buf)
+		os.Stdout.Write([]byte{'\n'})
+	case originalResponseType:
+		if req, ok := pendingRequests[reqID]; ok {
+			// Token is inside response body
+			req.OriginalResponse = payload
+		}
+	case replayedResponseType:
+		if req, ok := pendingRequests[reqID]; ok {
+			delete(pendingRequests, reqID)
+			req.MirroredResponse = payload
+
+			if !req.ResponseEquals() {
+				// TODO improve the message to at least print the requested path
+				// TODO in the future publish the results to S3 for easier processing
+				os.Stderr.WriteString("MISMATCH " + req.SerializeBase64() + "\n")
+			}
+		}
+	}
+}
---
+++ tools/goreplay-middleware/request.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/buger/goreplay/proto"
+)
+
+var horizonURLs = regexp.MustCompile(`"result_meta_xdr":[ ]?"([^"]*)",`)
+var findResultMetaXDR = regexp.MustCompile(`https:\/\/.*?stellar.org`)
+
+// removeRegexps contains a list of regular expressions that, when matched,
+// will be changed to an empty string. This is done to exclude known
+// differences in responses between two Horizon version.
+//
+// Let's say that next Horizon version adds a new bool field:
+// `is_authorized` on account balances list. You want to remove this
+// field so it's not reported for each `/accounts/{id}` response.
+var removeRegexps = []*regexp.Regexp{}
+
+type replace struct {
+	regexp *regexp.Regexp
+	repl   string
+}
+
+// replaceRegexps works like removeRegexps but replaces data
+var replaceRegexps = []replace{}
+
+type Request struct {
+	Headers          string
+	OriginalResponse []byte
+	MirroredResponse []byte
+}
+
+func (r *Request) ResponseEquals() bool {
+	// TODO fast fail on `Latest-Ledger` mismatch
+
+	originalBody := proto.Body(r.OriginalResponse)
+	mirroredBody := proto.Body(r.MirroredResponse)
+
+	return normalizeResponseBody(originalBody) == normalizeResponseBody(mirroredBody)
+}
+
+// normalizeResponseBody normalizes body to allow byte-byte comparison like removing
+// URLs from _links or tx meta. May require updating on new releases.
+func normalizeResponseBody(body []byte) string {
+	normalizedBody := string(body)
+	// `result_meta_xdr` can differ between core instances (confirmed this with core team)
+	normalizedBody = findResultMetaXDR.ReplaceAllString(normalizedBody, "")
+	// Remove Horizon URL from the _links
+	normalizedBody = horizonURLs.ReplaceAllString(normalizedBody, "")
+
+	for _, reg := range removeRegexps {
+		normalizedBody = reg.ReplaceAllString(normalizedBody, "")
+	}
+
+	for _, reg := range replaceRegexps {
+		normalizedBody = reg.regexp.ReplaceAllString(normalizedBody, reg.repl)
+	}
+
+	return normalizedBody
+}
+
+func (r *Request) SerializeBase64() string {
+	return fmt.Sprintf(
+		"headers: %s original: %s mirrored: %s",
+		r.Headers,
+		string(r.OriginalResponse),
+		string(r.MirroredResponse),
+	)
+}
