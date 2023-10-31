e627ef3223d58399523fa7100d5f7baa76bedf35
Urvi <urvi.savla@stellar.org>
2023-10-30 18:18:08 -0700

    Fix formatting

--- services/horizon/internal/ingest/processors/claimable_balances_change_processor_test.go
+++ services/horizon/internal/ingest/processors/claimable_balances_change_processor_test.go
@@ -7,11 +7,11 @@ import (
 	"testing"
 
 	"github.com/guregu/null"
-	"github.com/stretchr/testify/suite"
 	"github.com/stellar/go/ingest"
 	"github.com/stellar/go/services/horizon/internal/db2/history"
 	"github.com/stellar/go/support/db"
 	"github.com/stellar/go/xdr"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestClaimableBalancesChangeProcessorTestSuiteState(t *testing.T) {
