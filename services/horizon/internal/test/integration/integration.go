85e4fc768a1b3d29b820b15533ba794edb008ef8
Urvi <urvi.savla@stellar.org>
2025-03-17 22:21:56 -0700

    Remove stellar core running in in-memory mode. Also, remove the config parameter from the Horizon config, ingest library captive core config and any related code.

--- services/horizon/internal/test/integration/integration.go
+++ services/horizon/internal/test/integration/integration.go
@@ -660,14 +660,12 @@ func (i *Test) CreateCaptiveCoreConfig() (ledgerbackend.CaptiveCoreConfig, error
 		HistoryArchiveURLs:  []string{HistoryArchiveUrl},
 		NetworkPassphrase:   i.config.NetworkPassphrase,
 		CheckpointFrequency: CheckpointFrequency, // This is required for accelerated archive creation for integration test
-		UseDB:               true,
 		StoragePath:         i.CurrentTest().TempDir(),
 	}
 
 	tomlParams := ledgerbackend.CaptiveCoreTomlParams{
 		NetworkPassphrase:  i.config.NetworkPassphrase,
 		HistoryArchiveURLs: []string{HistoryArchiveUrl},
-		UseDB:              true,
 	}
 
 	toml, err := ledgerbackend.NewCaptiveCoreTomlFromFile(i.coreConfig.configPath, tomlParams)
